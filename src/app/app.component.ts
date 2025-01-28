fcf8c3c73008c18ca39ea73d0be5cad581f08082
Nikita Barkhatov <nikita.barkhatov@b2broker.com>
2025-01-28 18:12:46 +0400

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TooltipDirective } from './tooltip/tooltip.directive';
-import { OverlayModule } from '@angular/cdk/overlay';
 import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TooltipDirective, OverlayModule, NgTemplateOutlet],
+  imports: [RouterOutlet, TooltipDirective, NgTemplateOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
